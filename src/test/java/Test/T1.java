fbfcb16b47b7fe47b94b5d1a7f2b76a9eb6bd484
Ahmed <ahmedssp43.am@gmail.com>
2023-08-23 20:45:11 +0300

    initial commit

--- src/test/java/Test/T1.java
+++ src/test/java/Test/T1.java
@@ -13,15 +13,15 @@ public class T1 extends Setup {
         Reporter.log("________________Start testcase__________");
         //1- Instansiate soft assertion
         SoftAssert ass = new SoftAssert();
-        //1- assert we are in home page
+        //2- assert we are in home page
         ass.assertTrue(Home_P1.Assertion_Current_On_Homepage());
-        //2-search for "productname"
+        //3-search for "productname"
         Home_P1.Searchfor(productname);
-        //3-press botton to serach
+        //4-press botton to serach
         P_2_SearchResult P2_Search_Result=Home_P1.Press_search_botom();
-        //4-From the left hand side filter using "Transmission" -> "Manual"
+        //5-From the left hand side filter using "Transmission" -> "Manual"
         P2_Search_Result.press_manual_OPT();
-        //5-Validate the results obtained.
+        //6-Validate the results obtained.
                 //a- validate if number pf resulst equals to number of products
                  ass.assertTrue( P2_Search_Result.assertion_result_count());
                 //b-validate if all element in results contains the search keyword
